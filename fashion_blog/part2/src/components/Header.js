a76bd4cc73a204dc491dd98e247bb0005e047351
liujuan0616 <liujuan0616@gmail.com>
2023-12-13 13:39:05 -0500

    fashion-blog

---
+++ fashion_blog/part2/src/components/Header.js
@@ -0,0 +1,21 @@
+export default function Header(){
+    return(
+        <header>
+        <h1>Sartre's List</h1>
+        <h2>Better-Dressed People</h2>
+        <nav aria-label="Main Naviagtion" role="navigation">
+            <ul class="header-nav">
+                
+                <li><a href="#">Women's</a></li>
+                <li><a href="#">Men's</a></li>
+                <li><a href="#">On the Street</a></li>
+                <li><a href="#">The Catwalk</a></li>
+                <li><a href="#">AdWatch</a></li>
+                <li><a href="#">About</a></li>
+
+             </ul>
+        </nav>
+    </header>
+    )
+
+}
---
+++ fashion_blog/part2/src/components/Main.js
@@ -0,0 +1,25 @@
+export default function (){
+    return (
+        <main>
+        <article>
+            <time datetime="2020-11-12">11/12/20</time>
+            <h3>On the Street in Brooklyn</h3>
+             <img src="blog-image-1.jpg" alt="on the street in Brooklyn" />
+            <p>Cray culpa stumptown, flexitarian ex Odd Future do fugiat Wes Anderson proident 3 wolf moon officia bitters small batch. Et consequat do, nulla you probably haven’t heard of them High Life scenester. Vinyl fugiat High Life, kogi do VHS in aliqua lo-fi leggings gentrify Neutra tumblr. Odio vegan PBR, Marfa forage blue bottle try-hard readymade meggings retro delectus Echo Park. Ugh consectetur farm-to-table forage, laboris blue bottle McSweeney’s tattooed excepteur yr. Magna ut Schlitz flexitarian, vinyl craft beer proident yr forage 8-bit ethical sustainable placeat retro. Officia pickled beard, adipisicing gluten-free sint craft beer quis thundercats id 3 wolf moon fashion axe</p>
+            <aside>
+                <a href="#">Continues ...</a>
+            </aside>
+        </article>
+        
+        <article>
+            <time datetime="2020-11-11">11/11/20</time>
+            <h3>Vintage in Vogue</h3>
+            <img src="blog-image-2.jpg" alt="vintage in vogue" />
+            <p>Selfies sunt Tumblr, delectus small batch DIY umami sint. Polaroid chambray selfies McSweeney's Cosby sweater, Pitchfork tattooed assumenda Wes Anderson Blue Bottle twee Carles ennui. Nisi locavore fugiat sapiente salvia aliqua. Shoreditch kogi exercitation fashion axe. Wolf semiotics Pinterest, laboris quis master cleanse tousled small batch street art bespoke fingerstache dreamcatcher ethical labore. Dreamcatcher iPhone typewriter, tote bag four loko fanny pack master cleanse organic PBR reprehenderit actually. Craft beer drinking vinegar wayfarers, pork belly narwhal pariatur polaroid cupidatat in 8-bit distillery anim literally.</p>
+            <aside>
+                <a href="#">Continues ...</a>
+            </aside>
+        </article>
+    </main>
+    )
+}
